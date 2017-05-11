9a8c5219f45edcf47340eff74e7e0feb5b91c83d
haru067 <haru.pdf@gmail.com>
2017-05-12 02:21:02 +0900

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+dst="$HOME/dotfiles"
+
+if [ ! -e $dst/.git ]; then
+  echo "Cloning dotfiles..."
+  git clone https://github.com/haru067/dotfiles $dst
+else
+  echo "Updating dotfiles..."
+  (cd $dst && git pull)
+fi

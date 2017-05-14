fcd017db63d91e6d71c8477e83e312735ce92c24
haru067 <haru.pdf@gmail.com>
2017-05-14 20:34:50 +0900

    Add vim files

--- install.sh
+++ install.sh
@@ -4,12 +4,27 @@ set -euo pipefail
 dst="$HOME/dotfiles"
 
 if [ ! -e $dst/.git ]; then
-  echo "Cloning dotfiles..."
-  git clone https://github.com/haru067/dotfiles $dst
+    echo "Cloning dotfiles..."
+    git clone https://github.com/haru067/dotfiles $dst
 else
-  echo "Updating dotfiles..."
-  (cd $dst && git pull)
+    echo "Updating dotfiles..."
+    (cd $dst && git pull)
 fi
 
 echo "Creating symbolic links..."
 ln -si $dst/src/.gitconfig $HOME/.gitconfig
+ln -si $dst/src/.zshrc $HOME/.zshrc
+if [ "$(uname)" == 'Darwin' ]; then
+    #mac
+    ln -si $dst/src/_vimrc $HOME/_vimrc
+    ln -si $dst/src/_gvimrc $HOME/_gvimrc
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+    #linux
+    :
+elif [ "$(expr substr $(uname -s) 1 10)" == 'MINGW32_NT' ]; then
+    #cygwin
+    :
+else
+    echo "($(uname -a)) is not supported."
+    exit 1
+fi

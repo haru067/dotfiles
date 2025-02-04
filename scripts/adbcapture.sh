10f9242fdc8ee0f050fdaf07e62976de7a74a701
haru067 <haru.pdf@gmail.com>
2025-02-04 20:11:11 +0900

    Update adbcapture

--- scripts/adbcapture.sh
+++ scripts/adbcapture.sh
@@ -3,7 +3,8 @@
 tmpfile=$(mktemp)
 
 adb shell screencap -p > $tmpfile
-mogrify -resize 50% $tmpfile
+# mogrify -resize 50% $tmpfile
+mogrify -resize 300x $tmpfile
 osascript -e "set the clipboard to (read (POSIX file \"$tmpfile\") as JPEG picture)"
 
 rm $tmpfile
